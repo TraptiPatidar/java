6275c62cca4e9aef17b57cafe6f25c70061f44d4
Trapti Patidar <60178915+TraptiPatidar@users.noreply.github.com>
2021-02-21 19:34:21 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,16 @@
+package p1;
+public class sum{
+	public int c;
+	public sum(int a,int b){
+		c=a+b;
+	}
+	public void show(){
+		System.out.println("c="+c);
+	}
+}
+// class c1{
+// 	public static void main(String[] args) {
+// 		sum s=new sum(1,4);
+// 		s.show();
+// 	}
+// }
